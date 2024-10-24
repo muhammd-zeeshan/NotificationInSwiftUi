6b5f81a28c3ae9c87162a0895190ac79b213ddf1
muhmmad-zeeshan <rafaq657@gmail.com>
2024-10-25 01:04:30 +0500

    Initial Commit

---
+++ NotificationInSwiftUi/NotificationInSwiftUiApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotificationInSwiftUiApp.swift
+//  NotificationInSwiftUi
+//
+//  Created by Muhammad Zeeshan on 25/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct NotificationInSwiftUiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
